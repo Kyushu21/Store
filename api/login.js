d726afde9038e80ea04f29725ce6f6c177cbd603
Ruben Gonzalez <rumeles@live.com.mx>
2024-05-19 17:29:11 -0700

    update login api and users

--- api/login.js
+++ api/login.js
@@ -1,5 +1,5 @@
 
-const baseUrl = 'https://fakestoreapi.com/';
+const baseUrl = 'https://api.escuelajs.co/api/v1/auth/login';
 const users = {
 
     addUser: (event) => {
@@ -48,58 +48,11 @@ const users = {
             alert('Usuario registrado'+ json.id); 
             window.location.href='index.html';   
         })
-        .catch(error => console.log('Error: ', error));
+        .catch(error => {
+            
+        });
     },
-    
-    updateUser: (params) => {        
-        console.log(params);
-        // Update user in database
-        fetch('https://fakestoreapi.com/users',{
-            method:"PUT",
-            body:JSON.stringify(
-                {
-                    email: email,
-                    username: username,
-                    password: password,
-                    name:{
-                        firstname:name,
-                        lastname:'lastname'
-                    },
-                    address:{
-                        city:'kilcoole',
-                        street:'7835 new road',
-                        number:3,
-                        zipcode:'12926-3874',
-                        geolocation:{
-                            lat:'-37.3159',
-                            long:'81.1496'
-                        }
-                    },
-                    phone:'1-570-236-7033'
-                }
-            )
-        })
-            .then(res=>res.json())
-            .then(json=>console.log(json))
-            .catch(error=>console.log('Error: ', error));
-        res.status(200).json({ message: 'User updated' });
-    },
-
-    deleteUser: (params) => {
-        console.log(params);
-        fetch('https://fakestoreapi.com/users',{
-            method:"DELETE",
-            body:JSON.stringify(
-                {
-                    user: username
-                }
-            )
-        })
-            .then(res=>res.json())
-            .then(json=>console.log(json))
-            .catch(error=>console.log('Error: ', error));
-        res.status(200).json({ message: 'User deleted' });
-    },
+        
     loginUser: (event) => {
         event.preventDefault();
         const form = new FormData(event.target);
