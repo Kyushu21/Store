c6f807191741387d3650a59f7100eeb09c2ad7e1
LuisFranciscoSolano <luisfranciscosolano31@gmail.com>
2024-05-20 23:23:12 -0700

    Guardar cambios locales antes de fusionar main en Solano

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 //Ejecutando funciones
 document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
 document.getElementById("btn__registrarse").addEventListener("click", register);
@@ -62,3 +64,23 @@ anchoPage();
             caja_trasera_login.style.opacity = "1";
         }
 }
+
+const swiper = new Swiper('.swiper', {
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
