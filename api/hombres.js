1016a05a584b58ebe58c64943e4e85a5a75822a0
Ruben Gonzalez <rumeles@live.com.mx>
2024-05-22 14:07:13 -0700

    [+] add see details from mens

--- api/hombres.js
+++ api/hombres.js
@@ -112,7 +112,7 @@ const hombres = {
               </div>
               <div class="description-clothes">
                 <p>MXN $${element.price}</p>
-                <button onclick="verProducto('${element.id}')"class="buttonVermas">
+                <button onclick="hombres.verProducto('${element.id}')"class="buttonVermas">
                   Ver
                 </button>
               </div>
@@ -120,6 +120,9 @@ const hombres = {
             `;
         });
         productos.innerHTML = productosContent;
+    },
+    verProducto(id) {
+        window.location.href = `productoH.html?id=${id}`;
     }
 }
 
